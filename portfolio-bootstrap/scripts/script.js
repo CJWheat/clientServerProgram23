ee21a1b686492cb6bcaf3468eadcc0fb3b76b162
CJWheat <122296603+CJWheat@users.noreply.github.com>
2023-04-23 00:28:06 -0500

    Add files via upload

---
+++ portfolio-bootstrap/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Jason Wheat
+    File Name: script.js
+    Date: 04/19/2023
+*/
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function(){
+    $('.hero').height($(window).height());
+});
